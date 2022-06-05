69536253bf5e9a9e260f8fa5f2cf96809925d3c3
MorrisJ6 <44378342+MorrisJ6@users.noreply.github.com>
2022-06-05 02:18:23 +0200

    Update sentinel.py

--- sentinel.py
+++ sentinel.py
@@ -30,7 +30,7 @@ def main():
             else:
                 continue
     else: 
-        print("Slozka neexistuje s rastry s rozlisenim 10m neexistuje!")
+        print("Slozka obsahujici rastry s rozlisenim 10m neexistuje!")
         return
     f = os.path.exists(dirr20m)
     if f == True:     
@@ -63,11 +63,30 @@ def main():
             else:
                 continue
     else:
-        print("Slozka neexistuje s rastry s rozlisenim 20m neexistuje!")
-        return 
+        print("Slozka obsahujici rastry s rozlisenim 20m neexistuje!")
+        return
 
-    print(b2raster)
-    print(b11raster)
+    #print(b2raster)
+    #print(b11raster)
+
+    sarreader = "C:/Users/START/Desktop/!!!Data/S1A_IW_GRDH_1SDH_20210802T095223_20210802T095248_039049_049B89_FFDF.SAFE/measurement/"
+    f = os.path.exists(sarreader)
+    if f == True:    
+        directSAR = os.listdir(sarreader) # cteni slozky obsahujici Sentinel-1 snimky s rozlisenim 10m
+        for r in directSAR: # prohledani slozky obsahujici Sentinel-1 snimky
+            if r.startswith("s1a-iw-grd-hh"):
+                hh = r
+                hhpath = str(sarreader + hh) # cesta ke snimku s HH polarizaci
+                hhraster = rasterio.open(hhpath, driver = "GTiff") # cteni snimku s HH polarizaci
+            elif r.startswith("s1a-iw-grd-hv"):
+                hv = r
+                hvpath = str(sarreader + hv) # cesta ke snimku HV polarizaci
+                hvraster = rasterio.open(hvpath, driver = "GTiff") # cteni snimku s HV polarizaci
+    else:
+        print("Slozka obsahujici SAR snimky neexistuje!")
+        return
+
+    #print(hhraster, hvraster)
 
     print("Snimky nacteny")
 
