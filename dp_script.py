2c320a2c24de4f30c5804ce9d30defc5b9bebe70
MorrisJ6 <44378342+MorrisJ6@users.noreply.github.com>
2022-09-21 01:09:47 +0200

    Update dp_script.py

--- dp_script.py
+++ dp_script.py
@@ -1,28 +1,35 @@
-import rasterio as rio, numpy as np, os, scipy.ndimage, time
+import rasterio as rio, numpy as np, os, scipy.ndimage as sp, time, pandas as pd
 
 def main():
 
     """
-    Hlavni funkce programu. Nacte cestu k souborum s daty Sentinel-2, spusti casovac a funkci nacteni_dat_sentinel_2.
+    Hlavni funkce programu. Nacte cestu k souborum s daty Sentinel-2, spusti casovac a funkci nacteni_dat.
     """
 
-    starttime = time.perf_counter()
-    image_dir = "C:/Users/START/Desktop/!!!Data/S2A_MSIL2A_20210605T151911_N0300_R068_T22WEC_20210605T194737.SAFE/GRANULE/L2A_T22WEC_A031096_20210605T151910/IMG_DATA"
-    nacteni_dat_sentinel_2(image_dir)
-    stoptime = time.perf_counter()
-    doba_behu = format(((stoptime - starttime) / 60), '.2f')
-    print("Doba behu programu byla: {} minut".format(doba_behu))
+    starttime = time.perf_counter() # Spusteni casovace
+    image_dir = "C:/Users/START/Desktop/!!!Data/S2A_MSIL2A_20210605T151911_N0300_R068_T22WEC_20210605T194737.SAFE/GRANULE/L2A_T22WEC_A031096_20210605T151910/IMG_DATA" # Cesta ke slozce
+    train_samples_file = "C:/Users/START/Desktop/!!!Data/roi_body_tecka.csv"
+    nacteni_dat(image_dir, train_samples_file) # Zavolani funkce nacteni_dat
+    stoptime = time.perf_counter() # Zastaveni casovace
+    doba_behu = (stoptime - starttime) / 60 # Vypocet casu behu programu v minutach
+
+    if doba_behu >= 1.0: # Kosmeticka funkce, ktera rozhodne, zda cas vypsat v sekundach nebo minutach, hodiny snad nebudou potreba :)
+        doba_behu_m = format(doba_behu, '.2f') 
+        print("Doba behu programu byla: {} minut.".format(doba_behu_m))
+    else:
+        doba_behu_s = format(doba_behu * 60, '.2f')
+        print("Doba behu programu byla: {} sekund.".format(doba_behu_s))
     print("Program probehl uspesne.")
     exit(0)
 
-def nacteni_dat_sentinel_2(imagedir):
+def nacteni_dat(imagedir, train_samples_file):
 
     """
-    Vstupem je slozka obsahujici vstupni rastry Sentinel-2.
-    Vystupem jsou nactene rastry prevedene do numpy matic, ktere vstupuji do zavolene funkce vypoctu indexu. 
+    Funkce projde danou slozku, nacte rastry Sentinel-2 a prevede je do numpy matic. Zaroven pripravi trenovaci data pro dalsi praci.
+    Vstupem je slozka obsahujici vstupni rastry Sentinel-2 a csv soubor obsahujici trenovaci data.
+    Vystupem jsou nactene rastry prevedene do numpy matic, ktere vstupuji do zavolene funkce vypoctu indexu. Spolu s nimi pak pokracuji i trenovaci data.  
     """ 
 
-    imagedir = imagedir
     dirr10m = str(imagedir + "/R10m/") # cesta ke slozce obsahujici Sentinel-2 snimky s rozlisenim 10m
     dirr20m = str(imagedir + "/R20m/") # cesta ke slozce obsahujici Sentinel-2 snimky s rozlisenim 20m
 
@@ -69,42 +76,42 @@ def nacteni_dat_sentinel_2(imagedir):
                 b5path = str(dirr20m + b5) # cesta ke snimku modreho pasma
                 b5raster = rio.open(b5path, driver = "JP2OpenJPEG") # cteni snimku modreho pasma
                 rededge1reader = b5raster.read().astype("float32")
-                rededge1_zoom = scipy.ndimage.zoom(rededge1reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                rededge1_zoom = sp.zoom(rededge1reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 rededge1 = np.array(rededge1_zoom)
             elif r.endswith("_B06_20m.jp2"):
                 b6 = r
                 b6path = str(dirr20m + b6) # cesta ke snimku zelenho pasma
                 b6raster = rio.open(b6path, driver = "JP2OpenJPEG") # cteni snimku zeleneho pasma
                 rededge2reader = b6raster.read().astype("float32")
-                rededge2_zoom = scipy.ndimage.zoom(rededge2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                rededge2_zoom = sp.zoom(rededge2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 rededge2 = np.array(rededge2_zoom)
             elif r.endswith("_B07_20m.jp2"):
                 b7 = r
                 b7path = str(dirr20m + b7) # cesta ke snimku cerveneho pasma
                 b7raster = rio.open(b7path, driver = "JP2OpenJPEG") # cteni snimku cerveneho pasma
                 rededge3reader = b7raster.read().astype("float32")
-                rededge3_zoom = scipy.ndimage.zoom(rededge3reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                rededge3_zoom = sp.zoom(rededge3reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 rededge3 = np.array(rededge3_zoom)
             elif r.endswith("_B8A_20m.jp2"):
                 b8A = r
                 b8Apath = str(dirr20m + b8A) # cesta ke snimku blizkeho infracerveneho pasma
                 b8Araster = rio.open(b8Apath, driver = "JP2OpenJPEG") # cteni snimku blizkeho infracerveneho pasma
                 nir2reader = b8Araster.read().astype("float32")
-                nir2_zoom = scipy.ndimage.zoom(nir2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                nir2_zoom = sp.zoom(nir2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 nir2 = np.array(nir2_zoom)
             elif r.endswith("_B11_20m.jp2"):
                 b11 = r
                 b11path = str(dirr20m + b11) # cesta ke snimku cerveneho pasma
                 b11raster = rio.open(b11path, driver = "JP2OpenJPEG") # cteni snimku cerveneho pasma
                 swir1reader = b11raster.read().astype("float32")
-                swir1_zoom = scipy.ndimage.zoom(swir1reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                swir1_zoom = sp.zoom(swir1reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 swir1 = np.array(swir1_zoom)
             elif r.endswith("_B12_20m.jp2"):
                 b12 = r
                 b12path = str(dirr20m + b12) # cesta ke snimku blizkeho infracerveneho pasma
                 b12raster = rio.open(b12path, driver = "JP2OpenJPEG") # cteni snimku blizkeho infracerveneho pasma
                 swir2reader = b12raster.read().astype("float32")
-                swir2_zoom = scipy.ndimage.zoom(swir2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
+                swir2_zoom = sp.zoom(swir2reader, (1,2,2), order=0) #zmena velikosti pixelu z 20m na 10m
                 swir2 = np.array(swir2_zoom)
             else:
                 continue
@@ -112,27 +119,56 @@ def nacteni_dat_sentinel_2(imagedir):
         print("Slozka obsahujici rastry s rozlisenim 20m neexistuje!")
         exit(1)
 
-    vypocet_indexu(blue, green, red, nir1, rededge1, rededge2, rededge3, nir2, swir1, swir2)
+    train_samples = pd.read_csv(train_samples_file, sep = ";") # Cteni souboru s trenovacimi daty ve formatu csv
+    X = train_samples[["blue","green","red","rededge1","rededge2","rededge3","nir1","nir2","swir1","swir2","AWEInsh","AWEIsh","NDSI","NDWIICE","TCwet"]] # Sloupce obsahuji hodnoty pixelu trenovacich dat pro jednotlive pasma a indexy
+    y = train_samples["typ"] # Sloupec s typem landcoveru
+
+    vypocet_indexu(blue, green, red, nir1, rededge1, rededge2, rededge3, nir2, swir1, swir2, X, y) # Zavolani nasleduji funkce 
     return
 
-def vypocet_indexu(blue, green, red, nir1, rededge1, rededge2, rededge3, nir2, swir1, swir2):
+def vypocet_indexu(blue, green, red, nir1, rededge1, rededge2, rededge3, nir2, swir1, swir2, X, y):
 
     """
-    Vstupem jsou numpy matice z predchozi funkce.
-    Vystupem jsou predchozi matice doplnene nove vypocitanymi indexy opet ve forme matic.
+    Funkce vypocita 5 indexu z matic nactenych v predchozi funkci. Dale pak vytvori vicerozmernou matici, kterou prevede na 2D matici 
+    o velikosti [pocet pasem (15), pocet sloupcu krat pocet radku (120 560 400)]. Funkce zaroven postoupi trenovaci data dalsi funkci.
+    Vstupem jsou numpy matice z predchozi funkce a promenne vytvorene z trenovacich dat.
+    Vystupem je 2D matice tvorena vstupnimi maticemi spolu s nove vypocitanymi maticemi indexu, ktera vstupuje do funkce DUMMY spolu 
+    s trenovacimi daty.
     """
 
-    NDWIice = np.divide((blue - red), (blue + red), out = np.zeros_like(blue - red), where = (blue + red) != 0)
-    NDSI = np.divide((green - swir1), (green + swir1), out = np.zeros_like(green - swir1), where = (green + swir1) != 0)
-    TCwet = np.array(0.1509 * blue + 0.1973 * green + 0.3279 * red + 0.3406 * nir1 - 0.7112 * swir1 - 0.4572 * swir2, dtype = "float32")
-    AWEIsh = np.array(blue + 2.5 * green - 1.5 * (nir1 + swir1) - 0.25 * swir2, dtype = "float32")
-    AWEInsh = np.array(4 * (green - swir1) - (0.25 * nir1 + 2.75 * swir2), dtype = "float32")
+    rows = blue.shape[1]
+    cols = blue.shape[2]
+    array_1D = rows * cols # Vypocet jedne dimenze vektoru, prevedeni 2D matice do 1D radku hodnot
     
-    return blue, green, red, nir1, rededge1, rededge2, rededge3, nir2, swir1, swir2, NDWIice, NDSI, TCwet, AWEIsh, AWEInsh
+    NDWIice = np.divide((blue - red), (blue + red), out = np.zeros_like(blue - red), where = (blue + red) != 0) # Vypocet NDWice
+    NDSI = np.divide((green - swir1), (green + swir1), out = np.zeros_like(green - swir1), where = (green + swir1) != 0) # Vypocet NDSI
+    TCwet = np.array(0.1509 * blue + 0.1973 * green + 0.3279 * red + 0.3406 * nir1 - 0.7112 * swir1 - 0.4572 * swir2, dtype = "float32") # Vypocet TCwet 
+    AWEIsh = np.array(blue + 2.5 * green - 1.5 * (nir1 + swir1) - 0.25 * swir2, dtype = "float32") # Vypocet AWEIsh
+    AWEInsh = np.array(4 * (green - swir1) - (0.25 * nir1 + 2.75 * swir2), dtype = "float32") # Vypocet AWEInsh
     
+    matrix_original = np.stack((blue, green, red, rededge1, rededge2, rededge3, nir1, nir2, swir1, swir2, AWEInsh, AWEIsh, NDSI, NDWIice, TCwet), axis = 0) # Vytvoreni vicerozmerne matice obsahujici vsech pasma a indexy
+    bands = (matrix_original.shape[0])
+
+    matrix_reshape = matrix_original.reshape(bands, array_1D) # Transformace predchozi matice do 2D matice 
+
+    dummy(matrix_reshape, X, y) # Zavolani nasledujici funkce
+    return
+
+def dummy(matrix_reshape, X, y):
+    print("Dummy print")
+
+if __name__ == "__main__": 
+    main() # Zavolani programu
 
 
+    #
+    #print(blue[0, 15, 10])
+    #print(red[0, 1, 1])
+    #    
 
-
-if __name__ == "__main__": #Volani programu
-    main()
+    #
+    #print(matrix_reshape)
+    #print(matrix_reshape.shape)
+    #print(matrix_reshape[0, 153729])
+    #print(matrix_reshape[2, 10981])
+    #
